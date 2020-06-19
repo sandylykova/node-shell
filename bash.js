aaa0c3fed04101af7eb8fe72b2183cf90af626f3
Biligma Oyola <sandylykova94@mail.ru>
2020-06-19 13:12:47 -0400

    pwd part

--- bash.js
+++ bash.js
@@ -1,22 +1,16 @@
-process.stdout.write("prompt >");
-
-// process.stdin.on("data", (data) => {
-//   const cmd = data.toString().trim();
-
-//   process.stdout.write("You typed:" + cmd);
-//   process.stdout.write("\nprompt >");
-// });
+process.stdout.write("prompt > ");
 
 process.stdin.on("data", (data) => {
-  if (data === "pwd") {
-    const cmd = process.cwd().toString().trim();
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    const pwd = process.cwd().toString().trim();
 
-    process.stdout.write(cmd);
-    process.stdout.write("\nprompt >");
+    process.stdout.write(pwd);
+    process.stdout.write("\nprompt > ");
   } else {
     const cmd = data.toString().trim();
 
-    process.stdout.write("You typed:" + cmd);
-    process.stdout.write("\nprompt >");
+    process.stdout.write("You typed: " + cmd);
   }
+  process.stdout.write("\nprompt > ");
 });
