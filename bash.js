b27139fcffaee75bf77532a3871b7ff53e03f6b4
esk <emseely@gmail.com>
2020-06-19 14:35:07 -0400

    fixed issue

--- bash.js
+++ bash.js
@@ -2,15 +2,15 @@ process.stdout.write("prompt > ");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
+  if (cmd === "pwd") {
     const pwd = process.cwd().toString().trim();
 
     process.stdout.write(pwd);
-    process.stdout.write("\nprompt > ");
   } else {
     const cmd = data.toString().trim();
 
     process.stdout.write("You typed: " + cmd);
   }
+
   process.stdout.write("\nprompt > ");
 });
